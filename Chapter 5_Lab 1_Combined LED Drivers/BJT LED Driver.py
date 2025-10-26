69ae2e065dd76c205d0b5aa23450f422a1d7f7d9
Michael L. Bradford <michael.l.bradford1@gmail.com>
2025-10-25 19:16:42 -0500

    Create BJT LED Driver.py

---
+++ Chapter 5_Lab 1_Combined LED Drivers/BJT LED Driver.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import time
+
+class BjtDriver:
+    def __init__(self, pin=16, active_high=True):
+        self.active_high = active_high
+        self.out = Pin(pin, Pin.OUT, value=0 if active_high else 1)
+        self.off()
+
+    def on(self):
+        self.out.value(1 if self.active_high else 0)
+
+    def off(self):
+        self.out.value(0 if self.active_high else 1)
+
+    def pulse_ms(self, on_ms=100, off_ms=0):
+        self.on()
+        time.sleep_ms(on_ms)
+        self.off()
+        if off_ms:
+            time.sleep_ms(off_ms)
